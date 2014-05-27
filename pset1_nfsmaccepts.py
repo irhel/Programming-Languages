919f797372a19b11f954bb54081c098a4593e02e
Irhad Elezovikj <ielezovikj@gmail.com>
2014-05-27 10:53:02 -0700

    fix

--- pset1_nfsmaccepts.py
+++ pset1_nfsmaccepts.py
@@ -10,18 +10,19 @@ edges2 = { (1, 'a') : [1],
 accepting2 = [2]
 
 def nfsmaccepts(current, edges, accepting, visited):
-	if current in accepting:
-	    return ""
-	if current in visited:
-	    return None
-	else:
-	    possible_transitions = []
-	    for edge in edges:
-		    if edge[0] == current:
-			    for p in edges[edge]:
-				    res = nfsmaccepts(p, edges, accepting, visited + [current])
-					if res != None:
-					    return edge[1] + res
+        if current in accepting:
+                return ""
+        if current in visited:
+                return None
+        else:
+                possible_transitions = []
+                for edge in edges:
+                        if edge[0] == current:
+                                for p in edges[edge]:
+                                        res = nfsmaccepts(p, edges, accepting, visited + [current])
+                                        if res != None:
+                                                return edge[1] + res
+                                        
 print("Test 1: " + str(nfsmaccepts(1, edges, accepting, []) == "abc"))
 print("Test 2: " + str(nfsmaccepts(1, edges, [4], []) == "ab"))
 print("Test 3: " + str(nfsmaccepts(1, edges2, accepting2, []) == None))
