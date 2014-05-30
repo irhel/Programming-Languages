864b563364076a5c1fc6fb7cab04a6b14e1416c2
Irhad Elezovikj <ielezovikj@gmail.com>
2014-05-29 18:01:21 -0700

    pset2

--- pset2_hex.py
+++ pset2_hex.py
@@ -1,5 +1,7 @@
 import ply.lex as lex
+
 tokens = ('NUM', 'ID')
+
 def t_NUM_hex(token):
     r'0x[0-9a-f]+'
     s = token.value
@@ -14,19 +16,25 @@ def t_NUM_hex(token):
     token.value = res
     token.type = 'NUM'
     return token
+
 def t_NUM_decimal(token):
     r'[0-9]+'
     token.value = int(token.value)
     token.type = 'NUM'
     return token
+
 def t_ID(token):
     r'[a-zA-Z]+'
     return token
+
 t_ignore = ' \t\v\r'
+
 def t_error(t):
     print("Lexer: unexpecte character " + t.value[0])
     t.lexer.skip(1)
 lexer = lex.lex()
+
+
 s = "0x25 25823858235832 0x1 one two three"
 lexer.input(s)
 while True:
